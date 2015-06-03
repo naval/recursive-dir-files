1f985e539dc4358a99cf6ccf0c67b9bfbe0fd70e
naval patel <navalp@neuronsolutions.com>
2015-06-03 20:19:38 +0530

    added timeout

--- test/test.js
+++ test/test.js
@@ -5,12 +5,14 @@ var assert = require('assert')
 describe('Read recursive directories and files', function () {
     var start = path.join(__dirname, 'test_dir');
     it('Should return dirs and files', function (done) {
+        this.timeout(10000);
         recursiveDirFiles(start, function (err, dirs, files) {
             if (err) {
                 done(err);
             }
             assert.ok(dirs.length,2);
             assert.ok(files.length,2);
+            done();
         });
 
     });
